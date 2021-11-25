6cc33db986d736827aa6dbcf6a46f544d0946cb4
lorine <168425207@qq.com>
2021-11-25 19:33:13 +0800

    oss

--- src/Upload/Tencent/Tencent.php
+++ src/Upload/Tencent/Tencent.php
@@ -5,12 +5,25 @@ namespace Lorine\OssUtils\Upload\Tencent;
 
 
 use Lorine\OssUtils\Upload\ICloud;
+use Qcloud\Cos\Client;
 
 class Tencent implements ICloud
 {
-    public function uploadFile($config,$file,$fileName)
+    public function uploadFile($config, $file, $fileName)
     {
         // TODO: Implement uploadFile() method.
-    }
+        $cosClient = new Client(
+            array(
+                'region' => $config['region'],
+                'schema' => 'https', //协议头部，默认为http
+                'credentials' => array(
+                    'secretId' => $config['ak'],
+                    'secretKey' => $config['sk'])));
+        $res = $cosClient->Upload(
+                $bucket = $config['bucket'],
+                $key = $fileName,
+                $body = fopen($file, 'rb'));
 
+        return $res;
+    }
 }
