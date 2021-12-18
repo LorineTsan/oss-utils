2e409864237000d903e19166f4dca5797e2aab8e
lorine <168425207@qq.com>
2021-12-18 12:27:26 +0800

    返回值优化

--- src/Upload/Qiniu/Qiniu.php
+++ src/Upload/Qiniu/Qiniu.php
@@ -13,11 +13,21 @@ class Qiniu implements ICloud
     public function uploadFile($config,$tmpName,$fileName)
     {
         // TODO: Implement uploadFile() method.
-        $bucket = $config['bucket'];
-        $auth = new Auth($config['ak'],$config['sk']);
-        $token = $auth->uploadToken($bucket);
-        $uploadMgr = new UploadManager();
-        $res = $uploadMgr->putFile($token,$fileName,$tmpName);
-        return $res;
+        try {
+            $bucket = $config['bucket'];
+            $auth = new Auth($config['ak'],$config['sk']);
+
+            $token = $auth->uploadToken($bucket);
+            $uploadMgr = new UploadManager();
+            $res = $uploadMgr->putFile($token,$fileName,$tmpName);
+            if($res[1]==null){
+                $data = ['code'=>0,'msg'=>'success','data'=>$res[0]['key']];
+            }else{
+                $data = ['code'=>1,'msg'=>'error','data'=>$res[1]];
+            }
+        }catch (\Exception $e){
+            $data = ['code'=>1,'msg'=>'error','data'=>['result' => $e->getMessage()]];
+        }
+        return json_encode($data);
     }
 }
--- src/Upload/Tencent/Tencent.php
+++ src/Upload/Tencent/Tencent.php
@@ -12,18 +12,24 @@ class Tencent implements ICloud
     public function uploadFile($config, $tmpName, $fileName)
     {
         // TODO: Implement uploadFile() method.
-        $cosClient = new Client(
-            array(
-                'region' => $config['region'],
-                'schema' => 'https', //协议头部，默认为http
-                'credentials' => array(
-                    'secretId' => $config['ak'],
-                    'secretKey' => $config['sk'])));
-        $res = $cosClient->Upload(
+
+        try {
+            $cosClient = new Client(
+                array(
+                    'region' => $config['region'],
+                    'schema' => 'https', //协议头部，默认为http
+                    'credentials' => array(
+                        'secretId' => $config['ak'],
+                        'secretKey' => $config['sk'])));
+
+            $cosClient->Upload(
                 $bucket = $config['bucket'],
                 $key = $fileName,
                 $body = fopen($tmpName, 'rb'));
-
-        return $res;
+            $data = ['code'=>0,'msg'=>'success','data'=>$fileName];
+        }catch (\Exception $e){
+            $data = ['code'=>1,'msg'=>'error','data'=>['result' => $e->getMessage()]];
+        }
+        return json_encode($data);
     }
 }
